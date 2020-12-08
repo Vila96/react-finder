c83752227800b53594d9c4e0b60460e6d3ae2546
Tomas <tomas.vila.sanchez@gmail.com>
2020-12-09 00:40:24 +0100

    First try at finder

--- src/App.js
+++ src/App.js
@@ -8,18 +8,34 @@ import TaskList from './TaskList/TaskList'
 class App extends Component {
   constructor(props){
     super(props)
-    this.state = {
-        tasks: ["Clean bathroom", "Wash dishes", "Do the laundry", "Broom"],
+    this.list = {
+        tasks: ["Clean bathroom", "Wash dishes", "Do the laundry", "Broom"]
     };
+    this.state = this.list
+    
   }
     
+  componentDidMount(){
+  }
+
+  callMeBack = (e) => {
+    let regEx = new RegExp(`^${e.target.value}.*$`, "i");
+    this.setState(this.list.filter(element => {return regEx(element.text)}));
+  }
+
+  deleteFromList = (id) => {
+    console.log("Eliminar Tarea ", id);
+    this.list = this.list.filter(el => {return el.id !== id});
+    this.setState(this.list);
+  }
+
   render(){
     return (
       <div className="App">
         <header className="App-header">
 
-          <Finder />
-          <TaskList dataFromApp = {this.state.tasks}/>
+          <Finder callback = {this.callMeBack}/>
+          <TaskList dataFromApp = {this.state.tasks} callback = {this.deleteFromList}/>
           
         </header>
       </div>
